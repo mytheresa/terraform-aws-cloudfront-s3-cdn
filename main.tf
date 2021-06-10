3fda4ad0303e79cdb5b042e386d2f2ffb4c47e57
Alex Ruiz <Alex.ruiz-estradera@mytheresa.com>
2021-06-10 13:24:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "aws_s3_bucket" "origin" {
 }
 
 module "logs" {
-  source                   = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/0.15.1"
+  source                   = "git::https://github.com/cloudposse/terraform-aws-s3-log-storage.git?ref=tags/0.14.0"
   enabled                  = var.logging_enabled
   namespace                = var.namespace
   stage                    = var.stage
